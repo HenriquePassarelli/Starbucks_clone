fdf6e1ecf2c209cbfb6f808a52a8800c1528840c
henrique passarelli <henrique.passarellicc@gmail.com>
2021-06-23 17:47:04 -0300

    :tada:first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function Navfunction(x) {
+    x.classList.toggle("change");
+    /* let topnav = document.getElementById('nav');
+    if (topnav.style.display != "flex") {
+        topnav.style.display = "flex";
+    } else {
+        topnav.style.display = "none";
+    } */
+}
