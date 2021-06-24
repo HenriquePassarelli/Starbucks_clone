0ca156c94b41acfbd4c46af17ff622ea625972d1
henrique passarelli <henrique.passarellicc@gmail.com>
2021-06-23 23:01:34 -0300

    size

--- scripts.js
+++ scripts.js
@@ -1,9 +1,4 @@
 function Navfunction(x) {
     x.classList.toggle("change");
-    /* let topnav = document.getElementById('nav');
-    if (topnav.style.display != "flex") {
-        topnav.style.display = "flex";
-    } else {
-        topnav.style.display = "none";
-    } */
+   
 }
